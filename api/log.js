e4f1739349cd6e4bf0ea7e788d52f92f045663ae
fineshyt <agentshrmpy@gmail.com>
2026-02-13 05:54:02 -1200

    Refactor CORS handler and update redirect logic

--- api/log.js
+++ api/log.js
@@ -1,18 +1,16 @@
 // api/log.js
 export default function handler(req, res) {
-    // Enable CORS
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-    if (req.method === 'OPTIONS') {
-        res.status(200).end();
-        return;
-    }
-
-    // Redirect to main page with hash
-    const password = "Rat123";
-    const baseUrl = 'https://logger-67.vercel.app';
     
-    res.redirect(302, `${baseUrl}/#${password}`);
+    if (req.method === 'GET') {
+        // Redirect to main page with data in hash
+        const data = req.query.data;
+        if (data) {
+            res.redirect(302, `/#${data}`);
+        } else {
+            res.redirect(302, '/');
+        }
+    } else {
+        res.status(405).json({ error: 'Method not allowed' });
+    }
 }
